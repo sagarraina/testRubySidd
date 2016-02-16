1b6651976f490e308c56d5eb3ab0f7a20185cbc5
sagarraina <sags007@gmail.com>
2016-02-16 18:00:14 -0500

    Hello

--- app/controllers/test_contoller_controller.rb
+++ app/controllers/test_contoller_controller.rb
@@ -1,5 +1,7 @@
-class TestContollerController < ApplicationController
+class TestContollerController < A
+  pplicationController
   def testView
     @task="Hello world!"
+    @task = "Hey"
   end
 end
